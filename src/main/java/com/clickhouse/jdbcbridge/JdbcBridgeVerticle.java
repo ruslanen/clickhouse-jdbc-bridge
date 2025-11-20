78bc6178388715a3e86206dda59cf6d4da977292
ruslanen <ruslan68a@gmail.com>
2025-11-20 22:10:11 +0300

    Fix for blocking when executing long-waiting queries.

--- src/main/java/com/clickhouse/jdbcbridge/JdbcBridgeVerticle.java
+++ src/main/java/com/clickhouse/jdbcbridge/JdbcBridgeVerticle.java
@@ -256,20 +256,28 @@ public class JdbcBridgeVerticle extends AbstractVerticle implements ExtensionMan
 
         router.route("/metrics").handler(PrometheusScrapingHandler.create());
 
-        router.route().handler(BodyHandler.create()).handler(this::responseHandlers)
+        router.route().handler(this::responseHandlers)
                 .handler(ResponseContentTypeHandler.create()).failureHandler(this::errorHandler);
 
         // stateless endpoints
         router.get("/ping").handler(requestTimeoutHandler).handler(this::handlePing);
         router.get("/schema_allowed").handler(requestTimeoutHandler).handler(this::handleSchemaAllowed);
 
-        router.post("/identifier_quote").produces(RESPONSE_CONTENT_TYPE).handler(requestTimeoutHandler)
+        router.post("/identifier_quote").produces(RESPONSE_CONTENT_TYPE)
+                .handler(BodyHandler.create())
+                .handler(requestTimeoutHandler)
                 .handler(this::handleIdentifierQuote);
-        router.post("/columns_info").produces(RESPONSE_CONTENT_TYPE).handler(queryTimeoutHandler)
-                .handler(this::handleColumnsInfo);
-        router.post("/").produces(RESPONSE_CONTENT_TYPE).handler(queryTimeoutHandler).blockingHandler(this::handleQuery,
-                SERIAL_MODE);
-        router.post("/write").produces(RESPONSE_CONTENT_TYPE).handler(queryTimeoutHandler)
+        router.post("/columns_info").produces(RESPONSE_CONTENT_TYPE)
+                .handler(BodyHandler.create())
+                .handler(queryTimeoutHandler)
+                .blockingHandler(this::handleColumnsInfo, SERIAL_MODE);
+        router.post("/").produces(RESPONSE_CONTENT_TYPE)
+                .handler(BodyHandler.create())
+                .handler(queryTimeoutHandler)
+                .blockingHandler(this::handleQuery, SERIAL_MODE);
+        router.post("/write").produces(RESPONSE_CONTENT_TYPE)
+                .handler(BodyHandler.create())
+                .handler(queryTimeoutHandler)
                 .blockingHandler(this::handleWrite, SERIAL_MODE);
 
         log.info("Starting web server...");
